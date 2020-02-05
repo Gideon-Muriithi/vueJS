55f0add73cf0b730df3e9cbde2c05cf3d4211500
Gideon-Muriithi <gideongakenge@gmail.com>
2020-02-05 16:27:13 +0300

    Add more components

--- main.js
+++ main.js
@@ -21,28 +21,23 @@ Vue.component('product', {
             <li v-for="detail in details">{{ detail }}</li>
         </ul>
 
-        <div v-for="(variant, index) in variants" 
-        :key="variant.variantId"
+        <div v-for="(variant, index) in variants" :key="variant.variantId" 
         class="color-box"
         :style="{ backgroundColor: variant.variantColor }"
         @mouseover="updateProduct(index)">
         </div>
 
-        <button class="btn-sm btn-primary" v-on:click="addToCart" :disabled="!inStock"
-        :class="{ disabledButton: !inStock }">Add to Cart</button>
-
-        <div class="cart">
-            <p>Cart({{ cart }})</p>
-        </div>
+        <button class="btn-sm btn-primary" v-on:click="addToCart">Add to Cart</button>
     </div>
-</div>`,
+</div>
+<product-tabs></product-tabs>
+`,
 
 data () {
     return {
         brand: 'Vue Mastery',
         product: 'Socks',
         selectedVariant: 0,
-        inStock: true,
         link: 'https://www.hackerrank.com/challenges/30-data-types/problem',
         inventory: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
@@ -55,23 +50,29 @@ data () {
             {
                 variantId: 2233,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0
             }
         ],
 
-        cart: 0,
+        reviews: []
     }
 },
 
     methods:{
         addToCart: function () {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
 
         updateProduct(index) {
             this.selectedVariant = index
+        },
+        
+        addReview (productReview) {
+            this.reviews.push(productReview)
         }
     },
+    
 
     computed: {
         title () {
@@ -97,11 +98,90 @@ data () {
 })
 
 
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name">
+            </p>
+                
+            <p>
+                <label for="review">Review:</label>
+                <textarea id="review" v-model="review"></textarea>
+            </p>
+
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+
+
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+
+        </form>
+    `,
+
+    data () {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+
+    methods: {
+        onSubmit () {
+            let productReview = {
+                name: this.name, 
+                review: this.review,
+                rating: this.rating
+            }
+
+            this.$emit('review-submitted', productReview)
+            this.name = null,
+            this.review = null,
+            this.rating = null
+        }
+    }
+
+})
+
+
+Vue.component('product-tabs', {
+template: `
+    <div>
+        <span class="tab" v-for="(tab, index) in tabs" :key="index">{{ tab }}</span>
+    </div>
+`,
+
+data () {
+    return {
+        tabs: ['Reviews', 'Make a Review']
+    }
+}
+
+})
 
 var app = new Vue({
     el: '#app',
     data: {
-        premium: false
+        premium: false,
+        cart: []
+    },
+    methods: {
+        updateCart (id) {
+            this.cart.push(id)
+        }
     }
 })
 
